1ceba448d6d0e2b6f3f01695a31fb8a34082fb3a
Igor Bedesqui <igorbedesqui@gmail.com>
2024-02-09 15:46:13 +0000

    fix tailwind config, basic dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
+const dialog = () => {
+  return;
+};
+
+const Dialoguer = () => {
+  return (
+    <Dialog.Root open={true}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/20 data-[state=open]:animate-overlayShow fixed inset-0 backdrop-blur-sm" />
+        <Dialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <Dialog.Title className="text-lg">Edit profile</Dialog.Title>
+          <Dialog.Description className="mt-2.5 mb-5 leading-normal">
+            Make changes to your profile here. Click save when you're done.
+          </Dialog.Description>
+
+          <div className="mt-[25px] flex justify-end">
+            <Dialog.Close asChild>
+              <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
+                Save changes
+              </button>
+            </Dialog.Close>
+          </div>
+          <Dialog.Close asChild>
+            <button
+              className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+              aria-label="Close"
+            >
+              x
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
+
 function App() {
-  return <>hej do</>;
+  return (
+    <>
+      hej do
+      <Dialoguer />
+    </>
+  );
 }
 
 export default App;
