aa8046cf4f781f56479abd596f9ea91a61fe1d00
Igor Bedesqui <igorbedesqui@gmail.com>
2024-03-27 23:59:34 +0000

    refactor: extract dialoguer

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,7 @@
-import * as Dialog from "@radix-ui/react-dialog";
-
-import { useStore } from "zustand";
-import { createStore } from "zustand/vanilla";
 import { NestedThing } from "./NestedThing";
-
-interface DialogState {
-  open: boolean;
-  content: React.ReactNode;
-  title: string | null;
-  description: string | null;
-
-  triggerElement: HTMLElement | null;
-
-  setOpen: (open: boolean) => void;
-
-  reset: () => void;
-}
-
-const dialogStore = createStore<DialogState>((set) => ({
-  open: false,
-  content: null,
-  title: null,
-  description: null,
-
-  triggerElement: null,
-
-  setOpen: (open) => set({ open }),
-
-  reset: () =>
-    set({
-      open: false,
-      content: null,
-      title: null,
-      description: null,
-    }),
-}));
-
-export const dialog = ({
-  content,
-  title = null,
-  description = null,
-  triggerElement = null,
-}: {
-  content: DialogState["content"];
-  title?: DialogState["title"];
-  description?: DialogState["description"];
-  triggerElement?: DialogState["triggerElement"];
-}) => {
-  dialogStore.setState({
-    open: true,
-    content,
-    title,
-    description,
-    triggerElement,
-  });
-};
-
-const Dialoguer = () => {
-  const { open, content, title, description, setOpen, triggerElement, reset } =
-    useStore(dialogStore);
-
-  return (
-    <Dialog.Root
-      open={open}
-      onOpenChange={(newOpen) => {
-        setOpen(newOpen);
-        if (newOpen) return;
-        reset();
-      }}
-    >
-      <Dialog.Portal>
-        <Dialog.Overlay className="bg-gray-a-02 data-[state=open]:animate-overlayShow fixed inset-0 backdrop-blur-sm" />
-        <Dialog.Content
-          onCloseAutoFocus={() => {
-            console.log("onCloseAutoFocus", triggerElement);
-            triggerElement?.focus();
-            dialogStore.setState({ triggerElement: null });
-          }}
-          className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-gray-01 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
-        >
-          <Dialog.Title className="text-lg">{title}</Dialog.Title>
-          <Dialog.Description className="mt-2.5 mb-5 leading-normal">
-            {description}
-          </Dialog.Description>
-
-          {content}
-
-          <Dialog.Close asChild>
-            <button
-              className="absolute top-2 right-2 inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
-              aria-label="Close"
-            >
-              x
-            </button>
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
-  );
-};
+import { Dialoguer, dialog, resetDialog } from "./ui/dialog/Dialog";
 
 function App() {
-  const { reset } = useStore(dialogStore);
-
   return (
     <div className="min-h-screen grid place-items-center">
       <div className="flex flex-col gap-8 items-center justify-center">
@@ -119,7 +18,7 @@ function App() {
                   From App.tsx
                   <button
                     className="rounded-md bg-gray-03 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-04 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-07 w-fit"
-                    onClick={reset}
+                    onClick={resetDialog}
                   >
                     Close from content prop
                   </button>
---
+++ src/ui/dialog/Dialog.tsx
@@ -0,0 +1,108 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
+import { useStore } from "zustand";
+import { createStore } from "zustand/vanilla";
+
+interface DialogState {
+  open: boolean;
+  content: React.ReactNode;
+  title: string | null;
+  description: string | null;
+
+  triggerElement: HTMLElement | null;
+
+  setOpen: (open: boolean) => void;
+
+  reset: () => void;
+}
+
+const dialogStore = createStore<DialogState>((set) => ({
+  open: false,
+  content: null,
+  title: null,
+  description: null,
+
+  triggerElement: null,
+
+  setOpen: (open) => set({ open }),
+
+  reset: () =>
+    set({
+      open: false,
+      content: null,
+      title: null,
+      description: null,
+    }),
+}));
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const resetDialog = () => dialogStore.setState({ open: false });
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const dialog = ({
+  content,
+  title = null,
+  description = null,
+  triggerElement = null,
+}: {
+  content: DialogState["content"];
+  title?: DialogState["title"];
+  description?: DialogState["description"];
+  triggerElement?: DialogState["triggerElement"];
+}) => {
+  dialogStore.setState({
+    open: true,
+    content,
+    title,
+    description,
+    triggerElement,
+  });
+};
+
+/**
+ * Dialog provider;
+ * to be used by manipulating the state of the DialogStore.
+ */
+export const Dialoguer = () => {
+  const { open, content, title, description, setOpen, triggerElement, reset } =
+    useStore(dialogStore);
+
+  return (
+    <Dialog.Root
+      open={open}
+      onOpenChange={(newOpen) => {
+        setOpen(newOpen);
+        if (newOpen) return;
+        reset();
+      }}
+    >
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-gray-a-02 data-[state=open]:animate-overlayShow fixed inset-0 backdrop-blur-sm" />
+        <Dialog.Content
+          onCloseAutoFocus={() => {
+            console.log("onCloseAutoFocus", triggerElement);
+            triggerElement?.focus();
+            dialogStore.setState({ triggerElement: null });
+          }}
+          className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-gray-01 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
+        >
+          <Dialog.Title className="text-lg">{title}</Dialog.Title>
+          <Dialog.Description className="mt-2.5 mb-5 leading-normal">
+            {description}
+          </Dialog.Description>
+
+          {content}
+
+          <Dialog.Close asChild>
+            <button
+              className="absolute top-2 right-2 inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+              aria-label="Close"
+            >
+              x
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
