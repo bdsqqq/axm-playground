c4be6cc19497f2b86a3b4e26bff94df7061f1685
Igor Bedesqui <igorbedesqui@gmail.com>
2024-03-29 23:33:42 +0000

    feat(app): cycle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
+import { useCallback, useState } from 'react';
 import { ButtonShowcase, LatencyShowcase } from './ui/button/showcase';
 import { Dialoguer } from './ui/dialog/Dialog';
+import { Button } from './ui/button/button';
+import { Sun, Moon, CircleDotDashed } from './ui/icons';
+
+const THEMES = ['system', 'light', 'dark'] as const;
+const THEME_ICONS = {
+  system: <CircleDotDashed />,
+  light: <Sun />,
+  dark: <Moon />,
+} as const;
+
+const CycleToggleButton = ({
+  options,
+}: {
+  options: {
+    label: string;
+    icon: React.ReactNode;
+  }[];
+}) => {
+  const [selected, setSelected] = useState(0);
+  const cycleSelected = useCallback(() => {
+    setSelected((selected + 1) % options.length);
+  }, [selected, options.length]);
+
+  const Icon = options[selected]?.icon;
+
+  return (
+    // could add a context menu to select an item directly
+    <Button
+      data-valute={options[selected]?.label}
+      left={Icon}
+      onClick={cycleSelected}
+      variant="secondary"
+      size="sm"
+    />
+  );
+};
 
 function App() {
   return (
     <div className="grid min-h-screen place-items-center gap-8 p-12">
+      <CycleToggleButton
+        options={THEMES.map((theme) => ({
+          label: theme,
+          icon: THEME_ICONS[theme],
+        }))}
+      />
+      <hr className="w-full border-gray-06" />
       <LatencyShowcase />
       <hr className="w-full border-gray-06" />
       <ButtonShowcase />
