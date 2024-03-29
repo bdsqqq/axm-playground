6135065276e46d9299dc3c327407e75eb5075cc3
Igor Bedesqui <igorbedesqui@gmail.com>
2024-03-29 23:33:29 +0000

    feat(icons): add theme icons

--- src/ui/icons.tsx
+++ src/ui/icons.tsx
@@ -59,3 +59,70 @@ export const ChevronDown = ({ ...props }: React.ComponentProps<'svg'>) => (
     </defs>
   </svg>
 );
+
+export const Sun = ({ ...props }: React.ComponentProps<'svg'>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2.25"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    {...props}
+  >
+    <circle cx="12" cy="12" r="4" />
+    <path d="M12 2v2" />
+    <path d="M12 20v2" />
+    <path d="m4.93 4.93 1.41 1.41" />
+    <path d="m17.66 17.66 1.41 1.41" />
+    <path d="M2 12h2" />
+    <path d="M20 12h2" />
+    <path d="m6.34 17.66-1.41 1.41" />
+    <path d="m19.07 4.93-1.41 1.41" />
+  </svg>
+);
+
+export const Moon = ({ ...props }: React.ComponentProps<'svg'>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2.25"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    {...props}
+  >
+    <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+  </svg>
+);
+
+export const CircleDotDashed = ({ ...props }: React.ComponentProps<'svg'>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2.25"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    {...props}
+  >
+    <path d="M10.1 2.18a9.93 9.93 0 0 1 3.8 0" />
+    <path d="M17.6 3.71a9.95 9.95 0 0 1 2.69 2.7" />
+    <path d="M21.82 10.1a9.93 9.93 0 0 1 0 3.8" />
+    <path d="M20.29 17.6a9.95 9.95 0 0 1-2.7 2.69" />
+    <path d="M13.9 21.82a9.94 9.94 0 0 1-3.8 0" />
+    <path d="M6.4 20.29a9.95 9.95 0 0 1-2.69-2.7" />
+    <path d="M2.18 13.9a9.93 9.93 0 0 1 0-3.8" />
+    <path d="M3.71 6.4a9.95 9.95 0 0 1 2.7-2.69" />
+    <circle cx="12" cy="12" r="1" />
+  </svg>
+);
