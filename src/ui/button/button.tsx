beaec09ff688280a77fb25de7f4b537664a29c0a
Igor Bedesqui <igorbedesqui@gmail.com>
2024-04-01 11:57:19 +0100

    feat: start adapting to use that tsplay approach

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -4,11 +4,56 @@ import { Slot } from '@radix-ui/react-slot';
 import * as Toggle from '@radix-ui/react-toggle';
 import { type ToggleProps, PrimitiveButtonProps } from '@radix-ui/react-toggle';
 import { cva, type VariantProps } from 'class-variance-authority';
+import { UnionToIntersection } from 'ts-essentials';
 
 import { Link, LinkProps } from 'react-router-dom';
 
 import { cn } from '../util';
 import { Spinner } from '../icons';
+
+// IGOR, You were adapting your code to use https://www.typescriptlang.org/play?target=99&jsx=4&pretty=true#code/JYWwDg9gTgLgBAbzgYwB5wL5wGZQiOAImQBsBDAZwoFoA3MqYMgO2QFNqyBXGAC2mAwAnoQDcAKFCRYicXBzQA7gwAmAJTbYANHLgBhclQCCMGIwBGPNhR3yjrflAASAFQCyAGRNnglmNds4ACEeGAhmV09vCysbXQ0yZBgAOQgVNkCAMSVVNnVNHSxcfCIoNkSYMUlwaHgkAFVmYHCXCABJZn8oCjYk5uZMHDwCQhgaax7OphIKKvE2VGl4ZHCKeGwcqBVG-o1sOABeBShlLb24SjguZgBrZghFAcuAHgAFAD4ACl0VmuY2ToALjg33k8gA9OC4Hw2HAAAZlbBwuCKYAkEhwcywkDAKjAZgAcyGJV4bDKWkxPGOp3y+xUuIoZDA5W6cHxOB4XDKcDKFV0EKhFAgcF4LBUJHxRMEcAA+mRsF0ZdDSXAKGBeSo2RQOTAuRx1RAINh+XADWAKMDXoEBcrcaaGPA7bz0UI4AArLhrY7K2H4rrMMgYqQkNggAEwMgwfpwI0+lD4SD-TomyEo2ErLgkTWe2GEa53B7MQgXZia4TMi5UMlR1YXBVktMoUnIG5wekUZCMHEBzps5hrcoqE2I4GERHFhb+Uvam5sISx14m+QAfjg2ROuVpzyXYPkOxa7U6ZJ6fXC293F7BqWYyUz5HMIfPl+frwYZDDXQozwAoqgzBU3gAbTHTRCAAXQpT4ADoYIYAkLRLIRALAgBKQ53jgWgIGAFR3neQCAAYwJ3S93hIsEyOfOBKIvUdAF4NwBBPbgKDES1OAcTxQliQIM1ZkCNCDgwhIklSdIdAEjD1xpPY9ATcJwzed4JHmRZaj7LpsESWEpNUGS5KTGBFNkeRPl4y0UOBYSUjSNgJAhAAqez+XsuB6h6TV2XSSwCQJSV2ImMgCWsKC4AATQgLh2OAAleHgZRezCVU2EdGBnLgBYwAlZBBBIV1gH2edIvi+BEvbTKyFdMg23y7AyXDOAAzDY40q8rgfL8sAuUgHoKCg5y0oAAR6WEEAGiVbhFUxzUBSEQwJRIhBY8okjdXroAJcEVAgZAKHBXkkmoX5E3DKDYpAEgAGIyvIIRGthDw2Hm5BXSsuAABFtooDB+rBFyBoWd9MrYH75BcuFwbGVAQbgFyVn7eA3CEWS-nDYFMj0Q4QQk4zQbBMpdSgAZnnpWh3icNh0QgABCZ5NuAUm0u+360sR5Hjs6KDroq5J31hI4AHJEaMRRrHwNg2fkzp+bS8G4Wc8FdC5oQebDZdgTWRguIAHyuUtNHxPIJG+lSljbXpyG5O61S04JQnCV48HNHG-TJTT2DgexkEcEJTHtx2KBNScARUbUDEoChol8WJnkiLwHGgb8QzDTp3mtMFPccWPI78awY-cOOvYTpPwwwhATV4EdVR8Qk7LBVMK80NiAQimLvSq9tO1AfEWBgCk-DgNo4AlWcUUYKMuNGIRmWLS4YRwYBungM0Y32H2wmYFNBWFEWUR76FhSC+BGE1WM4XLNh1erglkWKAhxtbZfz+1MU4FnNgwB9Tf4XP9WuHMHF4A6z8OvZE4RcrejNA2YB4QTTnzVg1NgtAyS12NvIF2UA3awjXuEbBzAHYQHNIHP8wdQ6GAjqYGI-gvyx1wYnUMJc07yFwVnChUcqF508LQ4uKccbyAbtgeB-wkFQBQeIY2GU1Ln1tr7PB-tDi6B1rg-B5ooIZ2gEo-2CjpHr2Ub1XBGiCEUGUhImQcMvS4MxhsDcWx9zMD2M8Ax5ovimX9hSRE2My7yDMcsMhKs+YoFQJ8QgGxOjUHMBALMppqAACYeQRT1ioQgFJeJQVIOHPxKFlJoP2C4wxp1WJUwOEca46RsAGxUGhTxeNkpciJlVBAMEoK8SwIiA4CBERYDSVQPxbSukUD8VgcESldCoJ5DUwmcBnjQIGOfA4hBpnFgaTBZpYzsBtI6SgXxvNelbLDIM4ZGBMniHEN42UABGTGoJJm4PLq0wg4IKDRWYNQLgYBCAmnCAYYALY2mfEQeGDxO5TlynjlATG-yObIC5GUToLg4LJVrruVMYIAB6y5jmXlTANMY1AMq9BgLiqAeAoBAtWPAGU9wYC4OBDQu2zA6HJ3gEcCFMBUnQvDHCqAh9EWYCZtUtZCA-ncJgICy8wLWLMuFTym0qL0U7ixTivFB0yTEtJfDWUlLqVwFpTIhl9UdbMDvOCqVGKLynIjFy5Kxr6GdGXFBC13L5VQllSaDAfKaLyAAMpPKuGAXQtNcFkSOScslspYlHCuQ4ulsCp5sDmRQP+AD3m7k+VlG4vyWWirNaGkFhcwXMqQZC9lsL4UwGlXAZF8g0WmqRVCbF4xFj4sJaqsVObKVqKgDS-OHa9W9gLSdKFRKOWlp5W64cmgM3CqzbucVjdJU2rLU63c1al31txY25VRLoBqq9BSiAMAO1dqiKC3tgCGpGvnYyrJF4FUNuZJult2b1UOqtZe8MdqX2Lp3SVWN1rGUftjeWytcBq27jHfID1cBvUEgGK8-14JA3iCOUAA
+
+export const forwardUnionRef = React.forwardRef as unknown as <P>(
+  component: (
+    // the `ref` will be missing from here, but forwardRef dissapears in future react
+    // so handling it _after_ the spread is future-proof
+    props: P,
+    // this part is really just for the internal implementation of the component
+    // we could use "unknown" and type assertions after we check discriminant instead
+    ref: 'ref' extends keyof P
+      ? React.ForwardedRef<
+          UnionToIntersection<
+            NonNullable<
+              Parameters<Extract<P['ref'], (...args: unknown[]) => void>>[0]
+            >
+          >
+        >
+      : '🙁 .ref is missing from props'
+  ) => React.ReactNode
+) => ForwardRefComponent<P>;
+
+export interface ForwardRefComponent<P> {
+  (props: P): React.ReactNode;
+  /**
+   * Used in debugging messages. You might want to set it
+   * explicitly if you want to display a different name for
+   * debugging purposes.
+   *
+   * @see {@link https://legacy.reactjs.org/docs/react-component.html#displayname Legacy React Docs}
+   *
+   * @example
+   *
+   * ```tsx
+   *
+   * const MyComponent: FC = () => {
+   *   return <div>Hello!</div>
+   * }
+   *
+   * MyComponent.displayName = 'MyAwesomeComponent'
+   * ```
+   */
+  displayName?: string | undefined;
+}
 
 type TogglePropsWithoutPrimitiveButtonProps = Omit<
   ToggleProps,
@@ -175,7 +220,8 @@ interface BaseButtonProps {
 }
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends React.ClassAttributes<HTMLButtonElement>,
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
     BaseButtonProps,
     TogglePropsWithValuesAsNever,
     LinkPropsWithValuesAsNever,
