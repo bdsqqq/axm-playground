67ce594cf22008537c71cf9f947e79848cd60c80
Igor Bedesqui <igorbedesqui@gmail.com>
2024-04-08 13:02:30 +0100

    feat(util): add id function

--- src/ui/util.ts
+++ src/ui/util.ts
@@ -4,3 +4,20 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+import { customAlphabet } from 'nanoid';
+export const nanoid = customAlphabet(
+  '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+);
+
+const prefixes = {
+  portal: 'prtl',
+} as const;
+
+/**
+ *
+ * @see: https://unkey.dev/blog/uuid-ux thanks [@chronark_](https://twitter.com/chronark_)
+ */
+export function newId(prefix: keyof typeof prefixes): string {
+  return [prefixes[prefix], nanoid(16)].join('_');
+}
