940ad852dc0208e90cb5ecf6d710a71a2f4cc08a
Igor Bedesqui <igorbedesqui@gmail.com>
2024-05-22 22:05:23 +0100

    build: update million lint

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import MillionLint from '@million/lint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const plugins = [react()];
-plugins.unshift(MillionLint.vite());
 export default defineConfig({
-  plugins: plugins,
+  plugins: [react(), MillionLint.vite()],
 });
